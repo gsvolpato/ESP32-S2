dda730e44e2ff907cff628c639c2a115396da9c8
gsvolpato <gsvolpato@gmail.com>
2025-08-12 09:55:51 -0300

    Initial commit

---
+++ src/credentials_template.h
@@ -0,0 +1,24 @@
+#ifndef CREDENTIALS_TEMPLATE_H
+#define CREDENTIALS_TEMPLATE_H
+
+/*
+ * CREDENTIALS TEMPLATE
+ * 
+ * To use this project:
+ * 1. Copy this file and rename it to "credentials.h"
+ * 2. Fill in your actual credentials below
+ * 3. The credentials.h file is already in .gitignore so it won't be uploaded to GitHub
+ * 
+ * Get your Blynk credentials from: https://blynk.cloud/
+ */
+
+// Blynk Cloud credentials
+#define BLYNK_TEMPLATE_ID           "YOUR_TEMPLATE_ID_HERE"
+#define BLYNK_TEMPLATE_NAME         "YOUR_TEMPLATE_NAME_HERE"
+#define BLYNK_AUTH_TOKEN            "YOUR_AUTH_TOKEN_HERE"
+
+// WiFi credentials
+#define WIFI_SSID                   "YOUR_WIFI_SSID_HERE"
+#define WIFI_PASSWORD               "YOUR_WIFI_PASSWORD_HERE"
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include "credentials.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+#define BLYNK_PRINT Serial
+
+BlynkTimer timer;
+
+BLYNK_WRITE(V0)
+{
+  int value = param.asInt();
+  Blynk.virtualWrite(V1, value);
+}
+
+BLYNK_CONNECTED()
+{
+  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
+  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+}
+
+void myTimerEvent()
+{
+  Blynk.virtualWrite(V2, millis() / 1000);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASSWORD);
+
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
