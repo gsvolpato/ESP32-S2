cd4410f78cba2fe69e61e5e9732c987f2ca93872
Giovane <gsvolpato@gmail.com>
2025-08-12 11:27:09 -0300

    Added Blynk Terminal

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,48 @@ BLYNK_WRITE(V0)
   Blynk.virtualWrite(V1, value);
 }
 
+BLYNK_WRITE(V4)
+{
+  String message = param.asStr();
+  Serial.print("Terminal received: ");
+  Serial.println(message);
+  
+  if (message == "help") {
+    Blynk.virtualWrite(V4, "Available commands:\n");
+    Blynk.virtualWrite(V4, "- help: Show this help\n");
+    Blynk.virtualWrite(V4, "- status: Show system status\n");
+    Blynk.virtualWrite(V4, "- ip: Show IP address\n");
+    Blynk.virtualWrite(V4, "- uptime: Show uptime\n");
+    Blynk.virtualWrite(V4, "- restart: Restart ESP32\n");
+  }
+  else if (message == "status") {
+    Blynk.virtualWrite(V4, "System Status:\n");
+    Blynk.virtualWrite(V4, "WiFi: Connected\n");
+    Blynk.virtualWrite(V4, "Blynk: Connected\n");
+    Blynk.virtualWrite(V4, "Free Heap: " + String(ESP.getFreeHeap()) + " bytes\n");
+  }
+  else if (message == "ip") {
+    Blynk.virtualWrite(V4, "IP Address: " + WiFi.localIP().toString() + "\n");
+  }
+  else if (message == "uptime") {
+    unsigned long uptime = millis() / 1000;
+    Blynk.virtualWrite(V4, "Uptime: " + String(uptime) + " seconds\n");
+  }
+  else if (message == "restart") {
+    Blynk.virtualWrite(V4, "Restarting ESP32...\n");
+    delay(1000);
+    ESP.restart();
+  }
+  else {
+    Blynk.virtualWrite(V4, "Unknown command: " + message + "\n");
+    Blynk.virtualWrite(V4, "Type 'help' for available commands\n");
+  }
+}
+
 BLYNK_CONNECTED()
 {
-  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
-  Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
-  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+  Blynk.virtualWrite(V4, "ESP32-S2 Terminal Ready!\n");
+  Blynk.virtualWrite(V4, "Type 'help' for available commands\n");
 }
 
 void myTimerEvent()
